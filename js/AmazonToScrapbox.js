d38960bad62be60e9f268ccf78292806f26b47af
noratetsu <87066296+nora-tetsu@users.noreply.github.com>
2022-02-05 10:10:11 +0900

    Update AmazonToScrapbox.js
    自分用に取得項目をカスタマイズ

--- js/AmazonToScrapbox.js
+++ js/AmazonToScrapbox.js
@@ -1,57 +1,53 @@
- javascript:(function(){
- 	var p = document.getElementById("productTitle");//書籍のタイトルの処理
-    if (!p) var p = document.getElementById("ebooksProductTitle");
- 	var title = window.prompt('Scrap "Amazon" to your scrapbox.', p.innerText.trim());
-    if (!title) return;
-    title = '『'+ title +'』';
- 	var asin = document.getElementById('ASIN');//ASIN番号の処理
-  	if(asin){
-   		var a = 'ISBN:' + asin.value;
-  	}else{
-   		var asin = document.getElementsByName('ASIN.0')[0],a = 'ASIN:' + asin.value;
-    }
-    var detail = document.getElementById('detailBullets_feature_div');//出版社と出版年月の処理
-    if (!detail) {
-    	var subdoc = document.getElementById("product-description-iframe").contentWindow.document;
-     	var detail = subdoc.getElementById("productDetailsTable");
-    }
-    var detailtext = detail.innerText;
-    var pubdata = detailtext.match(/(出版社 : .+)(\(.+\))/);//[1]出版社:シーアンドアール研究所,[2](2018/7/27)
-    if (pubdata){
-       	pubdata[1] = pubdata[1].replace(/:/,':[');//出版社名をリンクにしないならこの2行は削除する
-       	pubdata[1] = (pubdata[1].match(/;/)?pubdata[1].replace(/;/,'];'):pubdata[1] + ']');
-        //pubdata[2] = pubdata[2] + ' ';//リンクなし
-        //pubdata[2] = pubdata[2].replace(/\((\d+)\//, '([$1]/') + ' ';//年をリンクに
-        pubdata[2] = pubdata[2].replace(/\((\d+\/\d+)\//, '([$1]/') + ' ';//年月をリンクに
-    }else{
-    	var pubdata = ['','',''];
-    }
-    var isbookDesc = document.getElementById("bookDescription_feature_div") != null
-   	if (isbookDesc){
-    	var decsdocP = document.getElementById("bookDescription_feature_div");//内容紹介の処理
-    	var decsdoc = decsdocP.firstElementChild;
-    	var d = decsdoc.firstElementChild;
-     		if (d){//内容紹介が存在しているなら
-     			var d1 = d.innerText.replace(/\n/g,'\n>');
-     		}else{
-     			var d1 = "";//内容紹介が空っぽの場合
-     		}
-    	}else{
-    	var d1 = "";//内容紹介が空っぽの場合
-    }
-	
-    var image=document.getElementById("imgBlkFront");//書影の処理
-    if (!image) var image = document.getElementById("ebooksImgBlkFront");
-    var imageurl = image.getAttribute("src");
-    var pub = [];//著者情報の処理
-  	var c = document.getElementsByClassName('author');
-  	for (g = 0; g < c.length ;g++){
-  		var at = c[g].innerText.replace(/\r?\n/g, '').replace(/,/,'');
-  		var pu = at.match(/\(.+\)/);
-  		var ct = at.replace(/\(.+\)/,'').replace(/ /g,'');
-  		pub.push(pu + ' [' + ct + ']');
-  	}
-  	var lines='['+imageurl+' '+window.location.href+']\n'  + pub.join(' ') + '\n' + pubdata[1] +  pubdata[2] + a + '\n>' + d1 + '\n[#書籍]\n[＊]\n[@]\n';//ページへの書き込み内容。ここで順番を変えればページ内容も変わります。
-    var body = encodeURIComponent(lines);
-    window.open('https://scrapbox.io/noratetsuobj/'+encodeURIComponent(title.trim())+'?body='+body)
- })();
+javascript:(function(){
+  // 書名を取得する
+  var ttl = document.getElementById("productTitle");
+  if (!ttl) var ttl = document.getElementById("ebooksProductTitle");
+  // 取得した書名をデフォルト値としてページタイトルを決める
+  var title = window.prompt('選択範囲を概要としてスクラップします。', ttl.innerText.trim());
+  if (!title) return;
+  // 『』をつける
+  title = '『'+ title +'』';
+
+  // サブタイトル（書名直後のバージョンと発売日が書いてある部分）を取得する
+  var subttl = document.getElementById("productSubtitle");
+  var sub;
+  if(subttl) {
+    sub = "\n" + subttl.innerText; // 後のために改行を付与　タグに囲まれたテキスト部分を取得
+  } else {
+    sub = ""; // サブタイトルが空のとき（undifined防止）
+  }
+
+  // 選択範囲を取得する
+  var select;
+  if (window.getSelection){ // 選択範囲があるとき
+    select = window.getSelection().toString();　// 選択範囲を文字列として取得
+    select = select.replace(/(\W+)( )(\W+)/g,'$1$3'); // 字間に時々紛れている半角スペースを除去
+    select = "\n> " + select.replace(/\n/g,'\n> '); // 行ごとに引用の「>」を付与
+  }else{
+    select = ""; // 選択範囲が空のとき（undifined防止）
+  };
+
+  // 書影を取得する
+  var image=document.getElementById("imgBlkFront");
+  if (!image) var image = document.getElementById("ebooksImgBlkFront");
+  var imageurl = image.getAttribute("src");
+
+  // 著者を取得する
+  var authors = [];
+  var c = document.getElementsByClassName('author');
+  for (g = 0; g < c.length ;g++){
+    var at = c[g].innerText.replace(/\r?\n|\t/g, '').replace(/,/,'');
+    var pu = at.match(/\(.+\)/);
+    var ct = at.replace(/\(.+\)/,'').replace(/ /g,'');
+    authors.push(pu + ' [' + ct + ']');
+  }
+
+  // 以上の情報を、実際に書き込みたい形に整えてひとつの文字列にまとめる
+  var lines = '['+imageurl+' '+window.location.href+']\n'  + authors.join(' ') + sub + '\n\n知った経緯\n \n' + select + '\n[#書籍]\n[＊]\n';
+
+  // URLの形にするためにエンコード
+  var body = encodeURIComponent(lines);
+
+  // 自分のプロジェクトのURLとして開いてページを作成（または同名のページに追記）
+  window.open('https://scrapbox.io/noratetsuobj/'+encodeURIComponent(title.trim())+'?body='+body)
+})();
